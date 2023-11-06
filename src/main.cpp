8cbb257bda760ff65a17e18942211c5c097ca484
atomicNum29 <atomicw63.546@gmail.com>
2023-11-06 21:23:06 +0900

    트리거링 감지 기능 추가

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,12 @@ bool sendGPSpacket(byte *packet, int len);
 #define gps Serial1
 int readGPS(byte *_data);
 
+// HALL SENSOR attatch on ADC2 (GP28, pin34)
+#define TRIG 28
+int readTrig();
+
+// green LED는 LoRa 송신이 일어날 때 점멸
+// blue LED는 트리거링이 감지되면 점등
 #define LED_G 14
 #define LED_B 15
 int b_state = 0;
@@ -37,6 +43,7 @@ void setup()
   pinMode(LED_G, OUTPUT);
   pinMode(LED_B, OUTPUT);
   pinMode(GDO0, INPUT);
+  pinMode(TRIG, INPUT);
 
   SPI.begin();         // mandatory
   lora.begin(433.4e6); // 433.4 MHz
@@ -47,33 +54,50 @@ void setup()
 
   gps.begin(9600);
 
-  digitalWrite(LED_G, HIGH);
+  digitalWrite(LED_B, LOW);
 }
 
 void loop()
 {
-  digitalWrite(LED_G, HIGH);
+  // digitalWrite(LED_B, HIGH);
   static byte _data[64] = {0};
   static int _data_len = 0;
   byte data[64];
   int len = readGPS(data);
+  int was_triggered = 0;
+
+  if (readTrig())
+  {
+    was_triggered = 1;
+    digitalWrite(LED_B, HIGH);
+  }
+  else
+  {
+    was_triggered = 0;
+    digitalWrite(LED_B, LOW);
+  }
+
   if (len)
   {
     strcpy((char *)_data, (const char *)data);
     _data_len = len;
-    Serial.write(data, len);
-    Serial.println();
+    // Serial.write(data, len);
+    // Serial.println();
     blink_led();
   }
+
   if (digitalRead(GDO0))
   {
     len = lora.getPacket(data);
     if (strncmp((const char *)data, ID, ID_LEN) == 0)
     {
-      if (sendGPSpacket(_data, _data_len))
+      byte packet[128] = {0};
+      _data[_data_len] = 0;
+      int packet_len = sprintf((char *)packet, "%d:%s", was_triggered, (char *)_data);
+      if (sendGPSpacket(packet, packet_len))
       {
         Serial.println("packet sent");
-        digitalWrite(LED_B, b_state ^= 1);
+        digitalWrite(LED_G, b_state ^= 1);
       }
     }
   }
@@ -116,7 +140,7 @@ bool sendGPSpacket(byte *packet, int len)
     return false;
 
   cnt++;
-  packet[len] = '\0';
+  // packet[len] = '\0';
   len = sprintf((char *)buf, "%04u::%s", cnt, (char *)packet);
   Serial.write(buf, len);
   Serial.println();
@@ -124,4 +148,10 @@ bool sendGPSpacket(byte *packet, int len)
 
   t = millis();
   return rtn;
+}
+
+int readTrig()
+{
+  int tmp = analogRead(TRIG);
+  return (tmp < 400 ? 1 : 0);
 }
